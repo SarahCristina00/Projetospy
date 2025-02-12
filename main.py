7bd2a9dad1e9cca543f13280196a7b4d443a3373
Sarah Cristina Freitas da Silva <163673649+SarahCristina00@users.noreply.github.com>
2025-02-12 17:36:17 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+print ('Calculadora simples ')
+
+numero1 = int(input('Digite o primeiro número: '))
+
+numero2 = int(input('Digite o segundo número: '))
+
+
+soma = numero1 + numero2;
+subtracao = numero1 - numero2;
+produto = numero1 * numero2;
+divisao = numero1 / numero2;
+resto = numero1 %numero2;
+print('\nCalculos: ');
+print('Soma: ',soma );
+print('subtracao: ', subtracao);
+print('produto: ', produto);
+print('divisão: ', divisao);
+print('resto: ', resto);
